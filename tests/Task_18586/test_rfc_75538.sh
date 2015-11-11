a9797c5bc6b3772e4e6d8408f4431eb83639976b
ttomttom <tamasb@nikhef.nl>
2015-11-11 14:03:42 +0100

    Froce numeric ports for netstat

--- tests/Task_18586/test_rfc_75538.sh
+++ tests/Task_18586/test_rfc_75538.sh
@@ -60,7 +60,7 @@ echo "---Test: PAP changes bind IP using localhost---"
 #########################################################
 
 echo "1) Testing if port 8150 is listen on hostname"
-netstat -l | grep `hostname`:8150 | grep LISTEN
+netstat -l --numeric-ports | grep `hostname`:8150 | grep LISTEN
 if [ $? -eq 0 ]; then
 	echo "OK" 
 else
@@ -75,7 +75,7 @@ fi
 echo "-------------------------------"
 
 echo "2) Testing if port 8151 is listen on localhost"
-netstat -l | grep localhost | grep 8151 | grep LISTEN
+netstat -l --numeric-ports | grep localhost | grep 8151 | grep LISTEN
 if [ $? -eq 0 ]; then
 	echo "OK" 
 else
